984e6eff87c291a87292a05400818abc79e1907d
matthewrobison818 <matthewrobison818@gmail.com>
2017-02-03 11:46:38 -0800

    Add js scripts to fade animals in on click

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $(".dog").fadeToggle();
+    $(".cat").fadeToggle();
+    $(".fish").fadeToggle();
+    $(".badger").fadeToggle();
+  })
+
+  $()
+})
